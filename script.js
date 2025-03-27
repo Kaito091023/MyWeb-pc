26c31a92fe84ec30cc26dc301f2d2ac74a387af1
Kaito091023 <akirodump130@gmail.com>
2025-03-28 01:27:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('scrollButton').onclick = function () {
+    window.scrollTo({
+        top: document.body.scrollHeight / 0,
+        behavior: 'smooth'
+    })
+};
+
+document.getElementById('scrollButton0').onclick = function () {
+    window.scrollTo({
+        top: 920,
+        behavior: 'smooth'
+    })
+};
+
+document.getElementById('scrollButton1').onclick = function () {
+    window.scrollTo({
+        top: 920,
+        behavior: 'smooth'
+    })
+};
+
+document.getElementById('scrollButton2').onclick = function () {
+    window.scrollTo({
+        top: 1900,
+        behavior: 'smooth'
+    })
+};
+
+document.getElementById('scrollButton3').onclick = function () {
+    window.scrollTo({
+        top: document.body.scrollHeight / 1,
+        behavior: 'smooth'
+    })
+};
